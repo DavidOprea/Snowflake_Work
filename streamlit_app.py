f882326d5e3afee8d3a429919de6439f86747c18
David Oprea <75183102+DavidOprea@users.noreply.github.com>
2026-04-18 14:41:54 -0400

    Clean up insert statement and update API URL
    Removed debug statement and fixed URL format.

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,12 +38,11 @@ if ingredients_list:
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                     values ('""" + ingredients_string + "','" + name + """')"""
 
-    st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f"Your Smoothie is ordered, {name}!")
 
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 st.text(smoothiefroot_response)
