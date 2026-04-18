de92033c7ea5c4c76c3dff0f522454ee2d724a93
David Oprea <75183102+DavidOprea@users.noreply.github.com>
2026-04-18 15:04:10 -0400

    Enhance smoothie selection with search functionality
    Added 'SEARCH_ON' column to the data selection and updated API call to use the search parameter.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 import requests
+import pandas as pd
 from snowflake.snowpark.functions import col, when_matched
 
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -12,7 +13,9 @@ st.write(
 
 cnx = st.connection('snowflake')
 session = cnx.session()
-my_dataframe = session.table('smoothies.public.fruit_options').select(col('FRUIT_NAME'))
+my_dataframe = session.table('smoothies.public.fruit_options').select(col('FRUIT_NAME'), col('SEARCH_ON'))
+
+pd_df = my_dataframe.to_pandas()
 
 name = st.text_input('Name on Smoothie')
 st.write('The name on your smoothie will be: ' + name)
@@ -28,8 +31,11 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, SEARCH_ON].iloc[0]
+      
         st.subheader(fruit_chosen + ' Nutitional Information')
-        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen}")  
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")  
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
